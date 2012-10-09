58822fc8e1828156303fd7b3534e63c6afed2c7e
Andrey Usov <ownport@gmail.com>
2012-10-09 07:04:25 +0300

    more sketches

--- massfetch.py
+++ massfetch.py
@@ -3,11 +3,19 @@
 #   Mass Fetch
 #
 
+# TODO specify JSON format for requests
+# TODO handle requests in JSON format
 import requests
 
 
 if __name__ == '__main__':
 
     import argparse
-
+    parser = argparse.ArgumentParser(description='massfetch text content')
+    parser.add_argument('-u', '--urls', help='url list')
+    parser.add_argument('-r', '--requests', help='request list')
+    parser.add_argument('-d', '--db', help='database uri')
     
+    args = parser.parse_args()
+    
+    print args
